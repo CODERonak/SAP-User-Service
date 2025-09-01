a108c808d374a5204ba1d9bbdb3e8e73f00df50a
r9988.2588 <r9988.2588@gmail.com>
2025-09-01 06:27:13 +0000

    implemented the exceptions

--- src/main/java/com/microservice/SAPUserService/service/UsersService.java
+++ src/main/java/com/microservice/SAPUserService/service/UsersService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import com.microservice.SAPUserService.UsersRepository;
 import com.microservice.SAPUserService.dto.*;
+import com.microservice.SAPUserService.exceptions.custom.DuplicateException;
+import com.microservice.SAPUserService.exceptions.custom.NotFoundException;
 import com.microservice.SAPUserService.mapper.UsersMapper;
 import com.microservice.SAPUserService.model.Users;
 
@@ -26,8 +28,9 @@ public class UsersService {
 
     public UserResponse registerUser(UserRequest request) {
         if (repository.existsByEmail(request.getEmail()))
-            throw new IllegalArgumentException(
-                    "Email already exists, if you want create a new account please use a different email");
+            throw new DuplicateException(
+                    "Email: " + request.getEmail()
+                            + " has already been taken, if you want to create a new account please use a different email");
 
         Users user = mapper.toModel(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
@@ -36,7 +39,8 @@ public class UsersService {
     }
 
     public UserResponse getUserById(Long userId) {
-        Users user = repository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Users user = repository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User not found with the id " + userId));
         return mapper.toResponse(user);
     }
 }
